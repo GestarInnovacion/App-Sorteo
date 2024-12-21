154ccb935f5230c603f585a2c04ace4d83927171
Alejandro Niño <alejandronino@gestarinnovacion.com>
2024-12-20 23:14:15 -0500

    Version Final

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,6 @@
 // 'https://hundred.com.co'
 
-const URL_BASE = 'http://localhost:8000';
+const URL_BASE = 'https://hundred.com.co';
 
 export const URL_LOGIN = `${URL_BASE}/login`;
 export const URL_LOGOUT = `${URL_LOGIN}/logout`;
